a21c49957530b1b875856b7ec7640bdcd96c93d8
Edison Yu <edison.isyu@gmail.com>
2019-06-02 11:34:09 -0400

    Update command_handler_cls_5.py

--- command_handler_cls_5.py
+++ command_handler_cls_5.py
@@ -23,7 +23,7 @@ class Tcp_Control :
         self.tcp_port = 29
         self.tcp_connected = 0
         self.tcpsock = None
-        
+
     def tcp_control_connect(self) :
         try:
             self.tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,7 +34,7 @@ class Tcp_Control :
             print("Success in TCP connection!")
             self.tcp_connected = 1
             time.sleep(0.5) ## Need??
- 
+
         except socket.error as msg :
             print("Error for connection!")
             self.tcp_connected = 0  # Error connection
@@ -47,13 +47,13 @@ class Tcp_Control :
             self.tcpsock.shutdown(1)
             self.tcpsock.close()
 
-        
+
     def tcp_send(self, tcp_data) :
         if self.tcp_connected == 1 :
             self.tcpsock.send(tcp_data.encode())
             time.sleep(0.1)
             #print(tcp_data)
-            
+
     def tcp_receive(self, tcp_data) :
         if self.tcp_connected == 1:
             self.tcpsock.settimeout(0.1)
@@ -99,7 +99,7 @@ class Video_Switch_File :
             with open(f_name) as json_file :
 
                 #print("\n------------- Raw data of json file ----------------")
-     
+
                 self.raw_data = json.load(json_file)
                 #print(self.raw_data)
 
@@ -164,7 +164,7 @@ class Video_Switch_File :
 
         while True :
             sw_id = input("- Enter \"id\" of the device: ")
-            sw_id = int(sw_id) 
+            sw_id = int(sw_id)
             if sw_in_out == "input" and sw_id <= self.in_num and sw_id > 0:
                 break
             elif sw_in_out == "output" and sw_id <= self.out_num and sw_id > 0:
@@ -172,13 +172,13 @@ class Video_Switch_File :
             else :
                 print("Wrong value entered!")
         sw_id_update = sw_id - 1
-        
+
         if sw_in_out == "input" : # 1.input
             one_device = self.input_list[sw_id_update]
-           
+
         elif sw_in_out == "output" : # 2.output
             one_device = self.output_list[sw_id_update]
-            
+
         print("--- Selected device info: ", one_device)
 
         print("- What data do you want to edit?")
@@ -194,14 +194,14 @@ class Video_Switch_File :
             sw_ip = input("- Enter ip address you want to change: ")
             one_device["host"] = sw_ip
             print(one_device["host"])
-            
-        print("--- Changed device info: ", one_device)            
-            
+
+        print("--- Changed device info: ", one_device)
+
         ## OverWrite file name
         self.switch_file_write(f_name)
         print("- Changed info is saved to the file, \"", f_name, "\"")
-        
-            
+
+
 
 ### Switch test
 class Switch_Test(Video_Switch_File, Tcp_Control) :
@@ -215,11 +215,11 @@ class Switch_Test(Video_Switch_File, Tcp_Control) :
     def v_file_read(self, file_name) :
         print("Trying to open json file of video switch, file_name=", file_name)
         self.switch_file_open(file_name)
-        
+
     def v_file_write(self, file_name) :
         print("Trying to write json file of video switch, file_name=", file_name)
         self.switch_file_write(file_name)
-        
+
     def v_file_edit(self, file_name) :
         self.switch_file_edit(file_name)
 
@@ -233,8 +233,8 @@ class Switch_Test(Video_Switch_File, Tcp_Control) :
         else :
             print("get command from user - connect tcp - receive/send command - want to send more?")
               # get a switching command - run - ...
-    
-    
+
+
     def auto_switch(self, none) :
         if self.in_num == 0 or self.out_num == 0 :
             #print("Error. Enter the correct video file name.")
@@ -247,14 +247,14 @@ class Switch_Test(Video_Switch_File, Tcp_Control) :
             input_num = 1
 
             while True:
-                
+
                 for x in self.input_list :
                     ip = x["host"]
                     print("\n",x["sysid"], ip)
-                    
+
                     cmd = "\rSPOASI%02d\r" % input_num
                     str_out = []
-                
+
                     # tcp connect
                     self.ip_address = ip
                     self.tcp_control_connect()
@@ -280,8 +280,8 @@ class Switch_Test(Video_Switch_File, Tcp_Control) :
                         input_num = 1
                     else :
                         input_num += 1
-            
-            
+
+
 
 ##### class: switch command #####
 #### switch command "SI", "SA", "SB" have dictionary to call each function
@@ -292,9 +292,9 @@ class Switch_Test(Video_Switch_File, Tcp_Control) :
 class comm_switch:
     def __init__(self, sw_com):
         self.sw_com = sw_com
-        
+
         self.list_switch_type = ["SI", "SA", "SB"]
-        
+
         self.input_command = { "SI": self.si_switch,
                                "SA": self.sa_switch,
                                "SB": self.sb_switch }
@@ -302,7 +302,7 @@ class comm_switch:
         self.in_num = 0
         self.switch_type = ""  # "SI", "SA", "SB"
 
-        
+
     def run_command(self):
         minus = 0 # check if minus value
         temp = self.sw_com.strip() # remove space the front
@@ -310,7 +310,7 @@ class comm_switch:
             sw_list = temp.split(x)
             if len(sw_list) == 2:   # check if "SI", "SA", "SB"
                 self.switch_type = x
-                self.out_num = int(sw_list[0]) # SPOxx 
+                self.out_num = int(sw_list[0]) # SPOxx
                 self.in_num = int(sw_list[1])   # SIyy
                 if self.out_num < 0 or self.in_num < 0: # check minus value
                     minus = 1
@@ -322,19 +322,19 @@ class comm_switch:
             print(self.out_num, self.in_num)
         else:
             print("Wrong command!")
-            
+
     #### functions of input_command, "SI", "SA", "SB"
     def si_switch(self, out_n, in_n):
         print("Audio and Video Output %02d switched to Input %02d." %(self.out_num, self.in_num))
 
     def sa_switch(self, out_n, in_n):
         print("Audio Only Output %02d switched to Input %02d." %(out_n, in_n))
-    
+
     def sb_switch(self, out_n, in_n):
         print("Video Only Output %02d switched to Input %02d." %(out_n, in_n))
-       
 
-    
+
+
 
 ##### Serial (RS232) Control Connection #####
 ### connect / send / receive / close
@@ -347,12 +347,12 @@ class Serial_Control :
         self.serial_obj = None
 
     def serial_control_connect(self) :
-        try :         
+        try :
             self.serial_obj = serial.Serial(port = self.serial_port,
                               baudrate = self.serial_b_rate,
                               bytesize = self.serial_bytesize,
                               timeout = 0.1)
-            self.serial_obj.close() # necessary??? before open? yes. 
+            self.serial_obj.close() # necessary??? before open? yes.
             print("Opening serial port...")
             self.serial_obj.open()
             print("Success in Serial connection")
@@ -361,7 +361,7 @@ class Serial_Control :
             print("Error for Serial connection")
             self.serial_connected = 0  # connection Error
             self.serial_close()
-            
+
     def serial_close(self) :
         if self.serial_connected == 1 :
             print("Serial Port Disconnected.")
@@ -448,7 +448,7 @@ class Serial_cls(Serial_Control) :
                 self.serial_receive(str_out)
             self.serial_close()
 
-                    
+
 
 
 
@@ -462,7 +462,7 @@ class Terminal(Tcp_Control) :
         self.tcpsock = None
         self.tcp_connected = 0
 
-    # set and check valid IP address 
+    # set and check valid IP address
     def ip_address(self, ip) :
         ip_list = ip.split(".")
         if len(ip_list) == 4 : # xxx.xxx.xxx.xxx (4 addr)
@@ -504,7 +504,7 @@ class Terminal(Tcp_Control) :
             str_out = []
             self.tcp_receive(str_out)
             print(str_out[0], end="")
-                
+
             while True :
                 str_com = input("")
                 if str_com == "exit" :
@@ -517,7 +517,7 @@ class Terminal(Tcp_Control) :
                 self.tcp_receive(str_out)
                 print(str_out[0], end="")
             self.tcp_close() # close tcp
-        
+
 
 ##### class: help command #####
 class Help :
@@ -559,7 +559,7 @@ class Help :
                     else :
                         x = ""
                 com_in += x
-          
+
 #            space = 0
 #            com_in = ""
 #            for x in str_in:
@@ -572,8 +572,8 @@ class Help :
 #                else :
 #                    com_in += x
 #                    space = 0
-                    
-            print (com_in)  # complete string of command     
+
+            print (com_in)  # complete string of command
             com_len = len(com_in)
             for x in help_list_command:
                 begin_index = com_in.find(x)
@@ -583,7 +583,7 @@ class Help :
                     cls_call(self) # call a class
                     break
 
-        
+
     def help_ip(self) :
         print("To enter ip address, type this:")
         print("ip address xxx.xxx.xxx.xxx   (xxx : 0 - 255 range)")
@@ -592,17 +592,17 @@ class Help :
         print("To enter tcp port, type this:")
         print("tcp port xx   (xx : tcp port number)")
 
-        
+
 help_list_command = { "ip address": Help.help_ip,
                       "tcp port": Help.help_tcp_port }
-              
+
 
 
 ##### class: status command #####
 class Status :
     def __init__(self) :
         self.my_status = "my status"
-        
+
     def run_status(self, none) :
         print("=============== Status =================")
         print(" RS232 : Baud Rate=%dbps, Data=%dbit, Parity=None, Stop=1bit"
@@ -610,10 +610,10 @@ class Status :
         print(" Serial Port = ", self.serial_port)
         print("-----------------")
         print(" Network Setting(Telnet Server) Status")
-        print(" Host IP Address = ", self.ip_address)        
+        print(" Host IP Address = ", self.ip_address)
         print(" TCP Port = ", self.tcp_port)
         print("-----------------")
-        
+
 
 
 
@@ -647,7 +647,7 @@ class Command(Help, Status, Terminal, Serial_cls, Switch_Test) :
 
 
 
-       
+
         print("\n======== Command List ========")
         print(" help\n help ip address\n help tcp port")
         print(" status")
@@ -671,9 +671,9 @@ class Command(Help, Status, Terminal, Serial_cls, Switch_Test) :
 
     def print_all(self, none) :
         print("--- Default member variables ---")
-        print(" help:", self.my_help)        
+        print(" help:", self.my_help)
         print(" status:", self.my_status)
-        print(" ip address:", self.ip_address)        
+        print(" ip address:", self.ip_address)
         print(" tcp port:", self.tcp_port)
         print(" serial baudrate:", self.serial_b_rate)
         print(" serial port:", self.serial_port)
@@ -684,7 +684,7 @@ class Command(Help, Status, Terminal, Serial_cls, Switch_Test) :
     def command_handler(self) :
         str_com = ""
         str_com = input("JK > ")
-        #str_com_len = len(str_com) 
+        #str_com_len = len(str_com)
         for x in list_command:
             begin_index = str_com.find(x)
             #list_com_len = len(x)
@@ -699,18 +699,18 @@ class Command(Help, Status, Terminal, Serial_cls, Switch_Test) :
 
 list_command = { "help": Help.run_help,
                  "status": Status.run_status,
-                 
+
                  "ip address": Terminal.ip_address,
                  "tcp port": Terminal.tcp_port,
                  "connect tcp": Terminal.tcp_connect,
-                 
+
                  "serial baudrate": Serial_cls.serial_baud, # data,parity,stop bits
                  "serial port": Serial_cls.serial_port,
                  "serial bytesize": Serial_cls.serial_bytesize,
                  "connect serial": Serial_cls.serial_connect,
-                 
+
                  "print": Command.print_all,
-                 
+
                  "read ksw" : Switch_Test.v_file_read,
                  "write ksw" : Switch_Test.v_file_write,
                  "edit ksw" : Switch_Test.v_file_edit,
@@ -719,10 +719,10 @@ list_command = { "help": Help.run_help,
                  "switch" : Switch_Test.switch_command_run }
 
 
-######### MAIN ########      
+######### MAIN ########
 comm = Command()
+
+print ("I love you so much, Jisook!!!")
 
 while True :
     comm.command_handler()
-    
-   
